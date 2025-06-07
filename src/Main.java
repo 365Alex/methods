00ebccf661f7da5e43864846b8a32b07a93bec92
master <aleshenko_ira_2017@mail.ru>
2025-06-07 21:05:18 +0300

    methods DZ2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,29 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void calculationCurrentYear(int year){
-        if (year > 1585){
-            if ((year % 400 ==0 && year % 100 !=0) || (year % 4 == 0 )){
-                System.out.println(year + " год является високосным");
-            }else {
-                System.out.println(year + " год не является високосным");
-            }
-        }
 
+    public static void isYearLeap(int year) {
+        if (year >= 1582 && ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)) {
+            System.out.println("Год " + year + " является високосным");
+        } else {
+            System.out.println("Год " + year + " не является високосным");
+        }
+    }
+    public static String calculatingDeliveryDays(int deliveryDistance) {
+        int timeOneZoneDelivery = 1;
+        String result;
+        if (deliveryDistance > 100) {
+            result = "Доставки нет";
+        } else if (deliveryDistance > 60) {
+            timeOneZoneDelivery += 2;
+            result = "Потребуется дней доставки: " + timeOneZoneDelivery;
+        } else if (deliveryDistance > 20) {
+            timeOneZoneDelivery += 1;
+            result = "Потребуется дней доставки: " + timeOneZoneDelivery;
+        } else {
+            result = "Потребуется дней доставки: " + timeOneZoneDelivery;
+        }
+        return result;
     }
     public static void installingOsAndYear(int clientOS, int clientDeviceYear){
         if (clientOS == 1 && clientDeviceYear <= 2015){
@@ -23,27 +37,14 @@ public class Main {
             System.out.println("Установите версию приложения для IOS по ссылке");
         }
     }
-    public static void calculateDeliveryDays(int deliveryDistance) {
-        int days = 1;
-        if(deliveryDistance >20 && deliveryDistance <=60){
-            days = days +1;
-
-        }else if (deliveryDistance >60 && deliveryDistance <=100){
-            days = days+2;
-
-        }else if(deliveryDistance >= 100) {
-            System.out.println("Свыше 100 км доставки нет");
-        }
-        System.out.println("Потребуется дней: " + days);
-    }
 
 
     public static void main(String[] args) {
         System.out.println("task 1");
-        calculationCurrentYear(2021);
+        isYearLeap(2021);
         System.out.println("task2 ");
         installingOsAndYear(0, 2014);
         System.out.println("task 3");
-        calculateDeliveryDays(95);
+        System.out.println(calculatingDeliveryDays(50));
     }
 }
